eb03baecc135b282b04bf1e3542cc896090e1452
Martha Fairbanks <marthafairbanks@gmail.com>
2016-09-07 02:00:18 -0400

    selectedWorks finished

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 
-// var waypoint = new Waypoint ({
-//   element: document.getElementsByClassName('waypoint'),
-//   handler: function(direction) {
-//     console.log('Scrolled to waypoint!');
-//   }
-// });
-
-
 $(document).ready(function() {
-    $(".rowIntro").addClass("show");
 
+  $(".rowIntro").addClass("show");
+
+  $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+  });
 	
 	$(".aboutMe").click(function() {
 		$(".rowIntro").toggleClass("hide show");
@@ -18,6 +17,27 @@ $(document).ready(function() {
 
 	$('.selectedWorksSlider').unslider({ 
 		dots: true,
-        arrows: true, 
+    arrows: true,
+    infinite: true,
+	});
+
+	function hideShow() {
+    	$(".rowRobot").addClass("hide");
+    	$(".rowSelectedWorks").addClass("show");
+    }
+
+	var waypoint = new Waypoint ({
+  		element: document.getElementsByClassName('rowRobot'),
+  		handler: function(direction) {
+  			if (direction !=='up') {
+    			console.log('Scrolled to waypoint 25%?!');
+    			$(".robot").addClass("moveRobby");
+    			$(".arrow").addClass("hide");
+    			$(".worksTitle").addClass("show");
+    			setTimeout(hideShow, 10000); 
+
+  			}
+  		},
+  		offset: '30%', 
 	});
 });
